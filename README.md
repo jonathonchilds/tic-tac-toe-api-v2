d1d7502c4979b496d31a1ba192d0e87bea0bfab5
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-12-23 19:30:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Here's my fork of SDG's tic-tac-toe api!
+
+The API's we used for our course were hosted on Heroku,
+and Heroku said goodbye.
+
+I want to use the app's I've proudly made, 
+so I learned how to deploy the API via fly !
---
+++ app.rb
@@ -0,0 +1,98 @@
+require "sinatra"
+require "sinatra/json"
+require "sinatra/activerecord"
+require "json"
+require "amazing_print"
+
+if ENV["PORT"]
+  set :port, ENV["PORT"]
+end
+
+if ENV["RACK_ENV"] != "production"
+  set :database_file, "./config/database.yml"
+end
+
+configure do
+  enable :cross_origin
+end
+
+before do
+  response.headers["Access-Control-Allow-Origin"] = "*"
+end
+
+options "*" do
+  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+
+  200
+end
+
+require_relative "./moves.rb"
+require_relative "./game.rb"
+
+get "/" do
+  send_file File.join(settings.public_folder, "docs.html")
+end
+
+post "/game" do
+  game = Game.new_game
+
+  json(game)
+end
+
+get "/game/:id" do
+  id = params[:id]
+  game = Game.find_by(id: id)
+  if game
+    json(game)
+  else
+    status 404
+  end
+end
+
+post "/game/:id" do
+  request.body.rewind
+  data = nil
+
+  begin
+    data = JSON.parse(request.body.read)
+  rescue JSON::ParserError
+    halt 400, json(error: "Sorry, your JSON request could not be parsed")
+  end
+
+  id = params[:id]
+
+  column = data["column"]
+  row = data["row"]
+
+  unless ["0", "1", "2"].include?(column.to_s)
+    halt 400, json(error: "The value #{column || "null"} is invalid for the parameter column")
+  end
+
+  unless ["0", "1", "2"].include?(column.to_s)
+    halt 400, json(error: "The value #{row || "null"} is invalid for the parameter row")
+  end
+
+  game = Game.find_by(id: id)
+  unless game
+    status 404
+    return
+  end
+
+  if game.winner
+    json(game)
+    return
+  end
+
+  result = game.human_move(row.to_i, column.to_i)
+
+  if result == :invalid
+    status 400
+    return
+  end
+
+  game.save
+
+  json(game)
+end
